81184a6bd3bbe5a4a7ab7674701da16698fc6fa1
victorsyang <victorsyang@tencent.com>
2020-05-28 16:53:32 +0800

    feat:复现代码

---
+++ src/pages/personal/personal.tsx
@@ -0,0 +1,8 @@
+import Taro from '@tarojs/taro';
+import { View } from '@tarojs/components';
+
+const Personal = () => (
+  <View>个人中心</View>
+);
+
+export default Personal;
---
+++ src/pages/solution/solution.tsx
@@ -0,0 +1,9 @@
+import Taro from '@tarojs/taro';
+import { View } from '@tarojs/components';
+
+const Solution = () => (
+  <View>问答列表</View>
+);
+
+export default Solution;
+
